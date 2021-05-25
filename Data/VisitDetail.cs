e9230f3174f50d82a20ea35bd15e8a353b459552
Sara Shoujaa <sara.shoujaa@ericsson.com>
2021-05-25 20:54:37 +0300

    NewDashboardFrontend

---
+++ Data/VisitDetail.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using IMK_web.Models;
+
+namespace Data{
+    public class VisitDetail{
+        public string SiteName { get; set; }
+        public string Country { get; set; }
+        public string User { get; set; }
+        public double RpiVersion { get; set; }
+        public double AppVersion { get; set; }
+        public String ASP { get; set; }
+        public String Date { get; set; }
+        
+    }
+}
