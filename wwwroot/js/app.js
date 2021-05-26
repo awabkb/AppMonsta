ab8c89bf5df1a022994e6897ca31718e8f7535ce
Sara Shoujaa <sara.shoujaa@ericsson.com>
2021-05-27 01:41:10 +0300

    AddTableActions

--- wwwroot/js/app.js
+++ wwwroot/js/app.js
@@ -323,7 +323,6 @@ function getData(startdate, enddate, countries, operators) {
             element.innerHTML = '';
             var data = [];
             for (var i in res) {
-                console.log(res[i]["name"])
                 res[i]["values"] = [res[i]["values"]]
                 data.push(res[i]);
             }
@@ -370,7 +369,6 @@ function getData(startdate, enddate, countries, operators) {
         type: "GET",
         data: Data,
         success: function (res) {
-            // document.querySelector('#table-content').innerHTML='';
             const tableDOM = document.querySelector('#site-details');
             tableDOM.innerHTML = '';
             const table = new eds.Table(tableDOM, {
@@ -412,9 +410,48 @@ function getData(startdate, enddate, countries, operators) {
                         sort: 'none'
                     },
                 ],
-                sortable: true
+                sortable: true,
+                actions: true,
+
+                onCreatedActionsCell: (td) => {
+                    td.innerHTML = `<button class="btn-icon email"><i class="icon icon-email"></i></button>`;
+
+                    td.querySelector('button.email').addEventListener('click', (evt) => {
+                        var tr = evt.target.closest('tr');
+                        var siteEngineer = $(tr).find('td').eq(2).text();
+
+                        $.ajax({
+                            url: "api/dashboardapi/unique_sites",
+                            type: "GET",
+                            data: siteEngineer,
+                            success: function (res) {
+                                const notification = new eds.Notification({
+                                    title: "Contact Info",
+                                    description: 'An email has been sent to you',
+                                });
+                                notification.init();
+                            }
+                        });
+                    });
+                }
             });
             table.init();
+
+            document.querySelector('#export-data').addEventListener('click', () => {
+                const notification = new eds.Notification({
+                  title: 'Export data',
+                  description: 'Table data is exported to IMK_Dashboard.csv file',
+                });
+                notification.init();
+                var rows = [];
+                rows.push(['Site Name', 'Country', 'Field Engineer', 'Android Version', 'IMK Version', 'ASP', 'Date']);
+                table.data.forEach(row => {
+                    rows.push([row["siteName"], row["country"], row["user"], row["appVersion"], row["rpiVersion"],row["asp"],row["date"]]);
+                    console.log(row);
+                });
+                exportToCsv("IMK_Dashboard.csv", rows)
+
+              });
         }
     });
 }
@@ -464,6 +501,46 @@ function mapData(result) {
 }
 
 
+function exportToCsv(filename, rows) {
+    var processRow = function (row) {
+        var finalVal = '';
+        for (var j = 0; j < row.length; j++) {
+            var innerValue = row[j] === null ? '' : row[j].toString();
+            if (row[j] instanceof Date) {
+                innerValue = row[j].toLocaleString();
+            };
+            var result = innerValue.replace(/"/g, '""');
+            if (result.search(/("|,|\n)/g) >= 0)
+                result = '"' + result + '"';
+            if (j > 0)
+                finalVal += ',';
+            finalVal += result;
+        }
+        return finalVal + '\n';
+    };
+
+    var csvFile = '';
+    for (var i = 0; i < rows.length; i++) {
+        csvFile += processRow(rows[i]);
+    }
+
+    var blob = new Blob([csvFile], { type: 'text/csv;charset=utf-8;' });
+    if (navigator.msSaveBlob) { // IE 10+
+        navigator.msSaveBlob(blob, filename);
+    } else {
+        var link = document.createElement("a");
+        if (link.download !== undefined) { // feature detection
+            // Browsers that support HTML5 download attribute
+            var url = URL.createObjectURL(blob);
+            link.setAttribute("href", url);
+            link.setAttribute("download", filename);
+            link.style.visibility = 'hidden';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        }
+    }
+}
 
 /////////////////////////////////////////////// GLOBES 
 var chart = am4core.create(document.getElementById("chartdiv"), am4maps.MapChart);
